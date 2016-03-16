b92cfec3e3d9107dc446c68c2c200ee060dccd3b
Ocean <ocean.yang@ihunuo.com>
2016-03-16 11:24:39 +0800

    增加react的支持

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
-var component = require('./component');
-var app = document.createElement('div');
+import './main.css';
 
-document.body.appendChild(app);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-app.appendChild(component());
+ReactDOM.render(<App />, document.getElementById('app'));
